287140d5defd4f6ae6b62d4d5876dbf4b79268f2
LSViana <lv201122@gmail.com>
2020-01-13 13:28:17 -0300

    Updates

---
+++ precache-manifest.c474045ab1fa6669873648353e1035b9.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5de1275076b335f5835e5f2cf91eec79",
+    "url": "/digitalk-test/index.html"
+  },
+  {
+    "revision": "441f3da490edc54f0731",
+    "url": "/digitalk-test/static/css/main.e3eac9e1.chunk.css"
+  },
+  {
+    "revision": "922e2f885e3ffa8743cb",
+    "url": "/digitalk-test/static/js/2.9e3ac873.chunk.js"
+  },
+  {
+    "revision": "e6ff33bd71c9fe5f55528ac2d613cee5",
+    "url": "/digitalk-test/static/js/2.9e3ac873.chunk.js.LICENSE"
+  },
+  {
+    "revision": "441f3da490edc54f0731",
+    "url": "/digitalk-test/static/js/main.653f6ae2.chunk.js"
+  },
+  {
+    "revision": "2410dde0919c0ec057f0",
+    "url": "/digitalk-test/static/js/runtime-main.74bea62d.js"
+  },
+  {
+    "revision": "a236f90f704b7c3626e4f41af1427975",
+    "url": "/digitalk-test/static/media/background-decoration-line.a236f90f.svg"
+  },
+  {
+    "revision": "e13234134271f9130920fdde4429e2bf",
+    "url": "/digitalk-test/static/media/blue-sensor.e1323413.svg"
+  },
+  {
+    "revision": "700eb952422965bc5deef5d3e3ff4492",
+    "url": "/digitalk-test/static/media/bug.700eb952.svg"
+  },
+  {
+    "revision": "8c7244ba7cd91bec41b0a9f5a7be094d",
+    "url": "/digitalk-test/static/media/dark.8c7244ba.svg"
+  },
+  {
+    "revision": "1ae91a5bff85711f80a333b01c05af89",
+    "url": "/digitalk-test/static/media/dragon.1ae91a5b.svg"
+  },
+  {
+    "revision": "67c45539a8a45202e09945935548cd74",
+    "url": "/digitalk-test/static/media/electric.67c45539.svg"
+  },
+  {
+    "revision": "c513c722a8e98dd9841f4ce5e81d3c6e",
+    "url": "/digitalk-test/static/media/fairy.c513c722.svg"
+  },
+  {
+    "revision": "9530ca7683c571718fe8a00a0dfabae6",
+    "url": "/digitalk-test/static/media/fighting.9530ca76.svg"
+  },
+  {
+    "revision": "638ac0cc7dea442f5389eb511a4e985a",
+    "url": "/digitalk-test/static/media/fire.638ac0cc.svg"
+  },
+  {
+    "revision": "c6f84c5d160d87b2fe1c8af20012c697",
+    "url": "/digitalk-test/static/media/flying.c6f84c5d.svg"
+  },
+  {
+    "revision": "67437e0268a15b4c0b7cca2095ef7bc8",
+    "url": "/digitalk-test/static/media/ghost.67437e02.svg"
+  },
+  {
+    "revision": "9a7e5f044ea8e91b055fa19da6a86429",
+    "url": "/digitalk-test/static/media/grass.9a7e5f04.svg"
+  },
+  {
+    "revision": "c73fdc2d71c4cf1ec7fe97307e3de61d",
+    "url": "/digitalk-test/static/media/green-rounded-button.c73fdc2d.svg"
+  },
+  {
+    "revision": "4a3160fd5041215dd1c4ee90f588b4da",
+    "url": "/digitalk-test/static/media/ground.4a3160fd.svg"
+  },
+  {
+    "revision": "6f70d7f3d6d816814c19b638ff8250d0",
+    "url": "/digitalk-test/static/media/ice.6f70d7f3.svg"
+  },
+  {
+    "revision": "ff9615333bd33aa7e93de73887aab95c",
+    "url": "/digitalk-test/static/media/left-marker.ff961533.svg"
+  },
+  {
+    "revision": "e9719d58cbd56e85d440e640ef4aa444",
+    "url": "/digitalk-test/static/media/normal.e9719d58.svg"
+  },
+  {
+    "revision": "8ef6014caa62221756292e309c1f98fd",
+    "url": "/digitalk-test/static/media/poison.8ef6014c.svg"
+  },
+  {
+    "revision": "5118a0933faadd1b00f27e9df9a8848b",
+    "url": "/digitalk-test/static/media/pokeball-solid-black.5118a093.svg"
+  },
+  {
+    "revision": "3fda20b5846552d0f30ac371a89c706f",
+    "url": "/digitalk-test/static/media/psychic.3fda20b5.svg"
+  },
+  {
+    "revision": "32a66b59ab99c40aca880e73aea9c8e3",
+    "url": "/digitalk-test/static/media/red-rounded-button.32a66b59.svg"
+  },
+  {
+    "revision": "082f2603e7f2d6c7eff0270173ebb20b",
+    "url": "/digitalk-test/static/media/rock.082f2603.svg"
+  },
+  {
+    "revision": "1684f16df120715455d493e35cd3aa97",
+    "url": "/digitalk-test/static/media/search.1684f16d.svg"
+  },
+  {
+    "revision": "b2124012d9c0f433ecc705530f59df12",
+    "url": "/digitalk-test/static/media/steel.b2124012.svg"
+  },
+  {
+    "revision": "d80faff905c30fb3fea4e19950d57962",
+    "url": "/digitalk-test/static/media/water.d80faff9.svg"
+  },
+  {
+    "revision": "61c156dce76f3eefd34433105e83abba",
+    "url": "/digitalk-test/static/media/yellow-rounded-button.61c156dc.svg"
+  }
+]);
